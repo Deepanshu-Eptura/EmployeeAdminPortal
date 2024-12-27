5188c7a1a898481b964cfa3d59c284a649c5b433
Deepanshu Saini <deepanshu.saini@eptura.com>
2024-12-27 21:35:14 +0530

    Add project files.

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,110 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // using this private we can access our Dbcontext
+        private readonly ApplicationDbContext dbContext;
+
+        //connecting to database
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //  READ method
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {   // 1st approach
+            //var allEmployees = dbContext.Employees.ToList();
+            //return Ok(allEmployees);
+
+            return Ok(dbContext.Employees.ToList()); //2nd approach
+        }
+
+        // get by id 
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();  // 404 respone i.e. NOT FOUND
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        // for this we require Dto in Models  i.e Data Transfer Object
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            // converting into Entity
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);  // 200 respone
+        }
+
+        // Update method
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();  // 404 respone i.e. NOT FOUND
+            }
+
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            dbContext.SaveChanges();
+
+            return Ok(employee);
+
+        }
+
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id) 
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges(); 
+            return Ok();
+        }
+    }
+}
---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        //{ 
+        //}
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=CO3182\\SQLEXPRESS;Database=EmployeesDb;Trusted_connection=true;TrustServerCertificate=true;");
+        }
+    }
+    
+}
